c2e6ac41336905d4f2c38d486f440aaf2b3f61d1
HaradaKumiko <fariv.fariv12@gmail.com>
2024-04-22 13:10:36 +0800

    editing main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"gofrendi/structureExample/appController"
 	"gofrendi/structureExample/appMiddleware"
 	"gofrendi/structureExample/appModel"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/mysql"
@@ -33,6 +34,9 @@ func main() {
 
 	// create new echo instant
 	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "<img style='display: block; margin: auto;' src='https://cdn.epicstream.com/images/ncavvykf/epicstream/a54b9c16f0f9e2de831b32febc169e734e4ded3d-1920x1080.png?rect=0,36,1920,1008&w=1200&h=630&auto=format'/>")
+	})
 	appMiddleware.AddGlobalMiddlewares(e)
 	appController.HandleRoutes(e, cfg.JwtSecret, personModel)
 
