9bffc4f1f58dfacfdc771cae5383751e65a99161
HaradaKumiko <fariv.fariv12@gmail.com>
2024-04-22 14:57:37 +0800

    editing deploy.yaml

--- appController/hello.go
+++ appController/hello.go
@@ -7,5 +7,5 @@ import (
 )
 
 func Hello(c echo.Context) error {
-	return c.String(http.StatusOK, "halo dunia 🌏")
+	return c.HTML(http.StatusOK, "<img style='display: block; margin: auto;' src='https://cdn.epicstream.com/images/ncavvykf/epicstream/a54b9c16f0f9e2de831b32febc169e734e4ded3d-1920x1080.png?rect=0,36,1920,1008&w=1200&h=630&auto=format'/>")
 }
